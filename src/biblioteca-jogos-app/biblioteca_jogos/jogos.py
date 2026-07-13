330129d35ac254184df0cffaa678bf0b085ba4cc
Nicholas155541 <nicholas.costa@escolar.ifrn.edu.br>
2026-07-13 17:27:44 +0000

    feat: adicionar cadastro de jogos (#5)

--- src/biblioteca-jogos-app/biblioteca_jogos/jogos.py
+++ src/biblioteca-jogos-app/biblioteca_jogos/jogos.py
@@ -1,7 +1,26 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
 
 bp = Blueprint("jogos", __name__)
 
+jogos = []
+
 @bp.route("/")
 def index():
-    return render_template("jogos/index.html")
+    return render_template("jogos/index.html", jogos=jogos)
+
+
+@bp.route("/criar", methods=("GET", "POST"))
+def criar():
+    if request.method == "POST":
+        jogo = {
+            "nome": request.form["nome"],
+            "plataforma": request.form["plataforma"],
+            "genero": request.form["genero"],
+            "status": request.form["status"]
+        }
+
+        jogos.append(jogo)
+
+        return redirect("/")
+
+    return render_template("jogos/criar.html")
